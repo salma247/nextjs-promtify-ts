0a3d79075d4d2f459a41d9e63741ddced22ef27f
salma247 <salmaelhamshary1@gmail.com>
2023-10-12 12:16:48 +0300

    feat: GET all prompts

---
+++ app/api/prompt/route.ts
@@ -0,0 +1,16 @@
+import Prompt from "@/models/prompt";
+import { connectToDB } from "@/utils/database";
+
+export const GET = async (req: Request) => {
+    try {
+        await connectToDB();
+
+        const prompts = await Prompt.find({}).populate("creator");
+
+        return new Response(JSON.stringify(prompts), { status: 200 });
+
+    } catch (error: any) {
+        console.log("Error getting prompts: ", error.message);
+        return new Response(JSON.stringify({ msg: error.message }), { status: 500 });
+    }
+}
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }: any) => {
+  return (
+    <div className="prompt_layout mt-16">
+      {data.map((post: any) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default function Feed() {
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(e.target.value);
+  };
+
+  const handleTagClick = (tag: string) => {
+    setSearchText(tag);
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch(`/api/prompt`);
+      const data = await res.json();
+
+      setPosts(data);
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          value={searchText}
+          required
+          onChange={handleSearchChange}
+          placeholder="Search for a tag or a username"
+          className="search_input peer"
+        />
+      </form>
+
+      <PromptCardList data={posts} handleTagClick={handleTagClick} />
+    </section>
+  );
 }
