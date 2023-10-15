248dfa384aa1f3a0d9d945aaccf96525f5d17dc2
salma247 <salmaelhamshary1@gmail.com>
2023-10-15 15:24:43 +0300

    fix: Post type

--- components/PromptCardList.tsx
+++ components/PromptCardList.tsx
@@ -2,7 +2,7 @@ import { Post, PostWithUser } from "@/types/global";
 import PromptCard from "./PromptCard";
 
 type Props = {
-  data: Post[];
+  data: PostWithUser[];
   handleTagClick?: (tag: string) => void;
   handleEdit?: (post: PostWithUser) => void;
   handleDelete?: (post: PostWithUser) => void;
@@ -11,7 +11,7 @@ type Props = {
 const PromptCardList = ({ data, handleTagClick, handleEdit, handleDelete }: Props) => {
   return (
     <div className="prompt_layout mt-16">
-      {data.map((post: any) => (
+      {data.map((post: PostWithUser) => (
         <PromptCard
           key={post._id}
           post={post}
