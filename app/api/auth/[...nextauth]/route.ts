1bb529d7d305e47bd9dd7b994c88e0c9b892c9f7
salma247 <salmaelhamshary1@gmail.com>
2023-10-15 17:34:19 +0300

    fix auth api

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,36 +1,15 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-import CredentialsProvider from "next-auth/providers/credentials"
 
 import User from '@/models/user';
 import { connectToDB } from '@/utils/database';
 
 const handler = NextAuth({
   providers: [
-    process.env.VERCEL_ENV === "preview"
-    ? CredentialsProvider({
-        name: "Credentials",
-        credentials: {
-          username: {
-            label: "Username",
-            type: "text",
-            placeholder: "jsmith",
-          },
-          password: { label: "Password", type: "password" },
-        },
-        async authorize() {
-          return {
-            id: 1,
-            name: "J Smith",
-            email: "jsmith@example.com",
-            image: "https://i.pravatar.cc/150?u=jsmith@example.com",
-          } as any;
-        },
-      })
-    : GoogleProvider({
-        clientId: process.env.GOOGLE_ID as string,
-        clientSecret: process.env.GOOGLE_SECRET as string,
-      }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    })
   ],
   callbacks: {
     async session({ session }) {
@@ -40,22 +19,24 @@ const handler = NextAuth({
 
       return session;
     },
-    async signIn({ account, profile, user, credentials }) {
+    async signIn({ profile }) {
       try {
         await connectToDB();
 
-        const userExists = await User.findOne({ email: profile?.email });
+        if (!profile) return false
+        const userExists = await User.findOne({ email: profile.email });
+
 
         if (!userExists) {
           await User.create({
-            email: profile?.email,
-            username: profile?.name?.replace(" ", "").toLowerCase(),
-            image: profile?.picture,
+            email: profile.email,
+            username: profile.name?.replace(" ", "").toLowerCase(),
+            image: profile.picture,
           });
         }
 
         return true
-      } catch (error : any) {
+      } catch (error: any) {
         console.log("Error checking if user exists: ", error.message);
         return false
       }
