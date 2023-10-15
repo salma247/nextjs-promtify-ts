f2bea9e5e6ac13757979eaf8426f8307b9e13c78
salma247 <salmaelhamshary1@gmail.com>
2023-10-15 13:55:18 +0300

    fix: filter instead of search API

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -2,28 +2,52 @@
 
 import { useState, useEffect } from "react";
 import PromptCardList from "./PromptCardList";
-import { Post } from "@/types/global";
+import { PostWithUser } from "@/types/global";
 
 export default function Feed() {
+  const [posts, setPosts] = useState<PostWithUser[]>([]);
+
   const [searchText, setSearchText] = useState("");
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [searchResults, setSearchResults] = useState<PostWithUser[]>([]);
+  const [searchTimeout, setSearchTimeout] = useState<NodeJS.Timeout | null>(
+    null
+  );
 
   const handleSearchChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    clearTimeout(searchTimeout!);
     setSearchText(e.target.value);
+
+    // Debounce search
+    setSearchTimeout(
+      setTimeout(() => {
+        const filteredPosts = filterPrompts(e.target.value);
+        setSearchResults(filteredPosts);
+      }, 500)
+    );
   };
 
   const handleTagClick = (tag: string) => {
     setSearchText(tag);
+
+    const filteredPosts = filterPrompts(tag);
+    setSearchResults(filteredPosts);
+  };
+
+  const filterPrompts = (searchText: string) => {
+    const regex = new RegExp(searchText, "i");
+    return posts.filter(
+      (post) =>
+        regex.test(post.prompt) ||
+        regex.test(post.tag) ||
+        regex.test(post.creator.username)
+    );
   };
 
   useEffect(() => {
     const fetchPosts = async () => {
       if (searchText) {
-        const res = await fetch(`/api/search/${searchText}`);
-        const data = await res.json();
-        setPosts(data);
-        console.log(data);
-        
+        const filteredPosts = filterPrompts(searchText);
+        setSearchResults(filteredPosts);
       } else {
         const res = await fetch(`/api/prompt`);
         const data = await res.json();
@@ -36,7 +60,10 @@ export default function Feed() {
 
   return (
     <section className="feed">
-      <form className="relative w-full flex-center" onSubmit={(e) => e.preventDefault()}>
+      <form
+        className="relative w-full flex-center"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <input
           type="text"
           value={searchText}
@@ -47,7 +74,10 @@ export default function Feed() {
         />
       </form>
 
-      <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      <PromptCardList
+        data={searchText ? searchResults : posts}
+        handleTagClick={handleTagClick}
+      />
     </section>
   );
 }
